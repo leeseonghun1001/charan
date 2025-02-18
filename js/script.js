1f9680e67124cbe1dfb6f84c30fcd7b4b24d685e
Lee Seong Hun <leeseonghun0641@gmail.com>
2025-02-18 17:07:50 +0900

    상단 제작 중

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-$(document).ready(function(){})
+$(document).ready(function () {
+  // 상단에 메뉴 호버 시 서브메뉴 슬라이드
+  $(".h-categoty").hover(
+    function () {
+    $(".menu-snb").stop().slideDown();
+    },
+    function () {
+    $(".menu-snb").stop().slideUp();
+    }
+  );
+});
